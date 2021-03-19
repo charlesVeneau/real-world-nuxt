ddb64a74b2dcc2a42c412598aecf50ea74ccb917
Charles Veneau <charles.veneau@gmail.com>
2021-03-19 11:27:54 +0100

    Integrate API calls and Setup Vuex

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  // Customize loading color bar
+  loading: { color: '#39b982' },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
